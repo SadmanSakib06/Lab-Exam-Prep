4dafd6bcb2b896f2ec474e245f19f7f5851e8934
SadmanSakib06 <80643863+SadmanSakib06@users.noreply.github.com>
2022-04-05 01:24:37 +0600

    Solved the Error in the Condition

--- Ques_B2/src/Robot.java
+++ Ques_B2/src/Robot.java
@@ -11,23 +11,23 @@ public class Robot {
 
     public void turnLeft(){
         if(direction.equals("E")) direction = "N";
-        if(direction.equals("N")) direction = "W";
-        if(direction.equals("W")) direction = "S";
-        if(direction.equals("S")) direction = "E";
+        else if(direction.equals("N")) direction = "W";
+        else if(direction.equals("W")) direction = "S";
+        else if(direction.equals("S")) direction = "E";
 
     }
     public void turnRight(){
         if(direction.equals("E")) direction = "S";
-        if(direction.equals("S")) direction = "W";
-        if(direction.equals("W")) direction = "N";
-        if(direction.equals("N")) direction = "E";
+        else if(direction.equals("S")) direction = "W";
+        else if(direction.equals("W")) direction = "N";
+        else if(direction.equals("N")) direction = "E";
 
     }
     public void move(){
         if(direction.equals("E")) location.x = location.x + 1;
-        if(direction.equals("W")) location.x = location.x - 1;
-        if(direction.equals("N")) location.y = location.y + 1;
-        if(direction.equals("S")) location.y = location.y - 1;
+        else if(direction.equals("W")) location.x = location.x - 1;
+        else if(direction.equals("N")) location.y = location.y + 1;
+        else if(direction.equals("S")) location.y = location.y - 1;
     }
     public Point getLocation(){
         return location;
